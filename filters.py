525ca08acc3e2edde7556b5aa39b116accc153f8
khabarova <93438029+khabarova@users.noreply.github.com>
2022-04-08 12:01:01 +0300

    Add files via upload

--- filters.py
+++ filters.py
@@ -5,9 +5,14 @@ from random import randint
 
 # токен бота
 bot = telebot.TeleBot('5211894904:AAFbuovu8W1VefPHBOffusDFwSppYqjB_0Q')
-# count_button, для того чтобы обрабатывать только один выбор и только один раз
+# count_button, для того чтобы обрабатывать 1 нажатие кнопки
 count_button = 0
-rand_id = randint(1, 10)
+# случайное id  для будущего
+rand_id1 = randint(1, 10)
+# случайное id для прошлого
+rand_id2 = randint(1, 10)
+
+
 # функция для вступления
 @bot.message_handler(content_types=['text'])
 def start(message):
@@ -30,7 +35,7 @@ def start(message):
         question = "Хотите начать?"
         bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
     if message.text == '/ready':
-        bot.register_next_step_handler(message, time)
+        time(message)
     if message.text != '/help' and message.text != '/start' and message.text != '/ready':
         bot.send_message(message.from_user.id, 'Я вас не понимаю( Напишите /help')
     if message.text == '/help':
@@ -50,7 +55,7 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Поехали',
                          reply_markup=types.ReplyKeyboardRemove(), parse_mode='Markdown')
         bot.send_message(call.message.chat.id, 'О! Вижу вы решили попытаться пройти квест. Удачи. Для начала выберите'
-                                               'время действий. Напишите /ready 2 раза по отдельности')
+                                               'время действий. Напишите /ready.')
 
     if call.data == "no" and count_button == 0:
         count_button += 1
@@ -60,19 +65,45 @@ def callback_worker(call):
     if call.data == "y" and count_button == 1:
         con = sqlite3.connect("base3.db")
         cur = con.cursor()
-
+        name1 = (cur.execute(f'SELECT name FROM user WHERE id={rand_id1}').fetchall())[0][0]
+        luck1 = cur.execute(f'SELECT luck FROM user WHERE id={rand_id1}').fetchall()[0][0]
+        authority1 = cur.execute(f'SELECT authority FROM user WHERE id={rand_id1}').fetchall()[0][0]
+        health1 = cur.execute(f'SELECT health FROM user WHERE id={rand_id1}').fetchall()[0][0]
         count_button += 1
         bot.send_message(call.message.chat.id, '''3100 год от Рождества Христово. Галактика Андромеды.
 Планета «NL-31» - ближайшая планета с климатом пригодным для жизни. Человеческая раса перебралась
 жить на эту планету из-за разрушения ядра планеты Земля. Время на планете «NL-31» течет в 2 раза
 медленнее земного, а вместо Солнца – звезда «Мабу».''')
         bot.send_message(call.message.chat.id, f'''Сегодня день Вашего рождения.
-Ваше имя - {(cur.execute(f'SELECT name FROM user WHERE id={rand_id}').fetchall())[0][0]}, 
-Ваша удача - {cur.execute(f'SELECT luck FROM user WHERE id={rand_id}').fetchall()[0][0]}, 
-Ваш авторитет- {cur.execute(f'SELECT authority FROM user WHERE id={rand_id}').fetchall()[0][0]}, 
-Ваше здоровье - {cur.execute(f'SELECT health FROM user WHERE id={rand_id}').fetchall()[0][0]}.''')
+Ваше имя - {name1}, 
+Ваша удача - {luck1}, 
+Ваш авторитет- {authority1}, 
+Ваше здоровье - {health1}.''')
+
     if call.data == "n" and count_button == 1:
+        con = sqlite3.connect("base3.db")
+        cur = con.cursor()
+        name2 = (cur.execute(f'SELECT name FROM user WHERE id={rand_id2}').fetchall())[0][0]
+        luck2 = cur.execute(f'SELECT luck FROM user WHERE id={rand_id2}').fetchall()[0][0]
+        authority2 = cur.execute(f'SELECT authority FROM user WHERE id={rand_id2}').fetchall()[0][0]
+        health2 = cur.execute(f'SELECT health FROM user WHERE id={rand_id2}').fetchall()[0][0]
         count_button += 1
+        bot.send_message(call.message.chat.id, '''Лучи палящего солнца неприятно светят вам в глаза.
+Шум голосов становится все громче. Вы жмуритесь сильнее, в попытках вернуться в сладкое царство морфея.
+Но тщетно. Злобная старуха Сафо - хозяйка гостиницы, под чьими окнами вы уснули- окатывает вас ледяной водой. 
+Не успев, прийти в себя, вы сталкиваетесь с возмущением хозяйки. "А ка зараза ко мне повадилась!
+Проку от тебя ноль. Запомни уже, я тебя давно уволила. Не смей, больше сюда приходить, только портишь вид
+моему заведению!!! В следующий раз вылью кипящую смолу!!!" - прокричала вам карга Сафо. Мда...
+Судьба у вас не завидная. Хотя кому щас легко? На дворе 675 г. до н.э., Греция, остров Крит. Сильная засуха,
+голод, нехватка работы из-за большого количество захваченных рабов. Конечно же, им то платить не надо: 
+дал кувшин воды, да яблок штук 7 и ходят себе счастливые.... Ну да ладно, начался новый день, 
+а значит надо снова отправляться на поиски новый работы!.''')
+        # из бд выводим имя и остальные параметры, соответствующие id
+        bot.send_message(call.message.chat.id, f''' Ваша краткая биография.
+Имя - {name2}, 
+Уровень удачи - {luck2}, 
+Авторитет- {authority2}, 
+Уровень здоровья - {health2}.''')
 
 
 def time(message):
